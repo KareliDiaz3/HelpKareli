647533df84bf1f267419d200280beba0293982f1
kareli <k-reli@hotmail.com>
2025-11-15 13:29:35 -0600

    Primer commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -12,7 +12,7 @@ const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'Karamelo3!!',
   database: process.env.DB_NAME || 'SpeakLexi2',
   waitForConnections: true,
   connectionLimit: 10,
